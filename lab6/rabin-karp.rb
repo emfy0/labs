df5792469bc2a797cba7d903b3a86c0ad02b594b
Pavel Egorov <pavel.egorov@hodlhodl.com>
2023-03-26 22:37:20 +0300

    asd

---
+++ lab6/rabin-karp.rb
@@ -0,0 +1,19 @@
+require 'cityhash'
+
+class String
+  def rk_search(substring)
+    substring_hash = CityHash.hash64(substring)
+    substring_size = substring.size
+
+    each_byte.with_index do |_, i|
+      return if i + substring_size > size
+      return i if CityHash.hash64(self[i, substring_size]) == substring_hash
+    end
+  end
+end
+
+n         = 50000
+string    = 'hello world'
+substring = 'world'
+
+string.rk_search('world')
